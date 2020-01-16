5a36ff6e173a4bb813ab5c924acce4e82f9941f9
jjcenturion <jjcenturion00@gmail.com>
2020-01-16 18:20:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+cont_letra = 0
+cont_palabra = 0
+cadena = None
+
+while cadena != '.'
+	cadena = input('Ingresar letra, termina con"."')
+	cont_letra += 1
+
+	if cadena == '' or cadena == '.'
+		if cont_letra > 1
+			cont_palabra +=1
+  
+		cont_letra = 0
+
+print('Cantidad de palabras:', cont_palabra )		
