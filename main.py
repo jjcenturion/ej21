46f21796bbf1663f03822af4978f8ae0ab53901b
jjcenturion <jjcenturion00@gmail.com>
2020-01-16 20:15:33 +0000

    corrección de sintaxis

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ cont_letra = 0
 cont_palabra = 0
 cadena = None
 
-while cadena != '.'
+while cadena !='.':
 	cadena = input('Ingresar letra, termina con"."')
 	cont_letra += 1
 
-	if cadena == '' or cadena == '.'
-		if cont_letra > 1
+	if cadena == '' or cadena == '.':
+		if cont_letra > 1:
 			cont_palabra +=1
   
 		cont_letra = 0
