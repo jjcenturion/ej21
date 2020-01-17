6b7684764b6fc6f740cfc70d97ea806b0eab1164
jjcenturion <jjcenturion00@gmail.com>
2020-01-17 17:59:04 +0000

    Se corrigió error de indectacion, cuidado al conf. se dejo tabs 2( no cambiar en durante la escritura, configurarlo al inicio). El espacio se debe dar comillas, espacio, comillas.

--- main.py
+++ main.py
@@ -2,25 +2,23 @@ cont_letra = 0
 cont_palabra = 0
 cont_palabras_p = 0
 cadena = None
-flag_p = False
 
 while cadena !='.':
 	cadena = input('Ingresar letra, termina con"."')
 	cont_letra += 1
-   
-    #Primera letra que comienza con p. 
-  if cont_letra == 1 and cadena == 'p':
-    flag_p = True
 
-	if cadena == '' or cadena == '.': # Fin de palabra
+	if cadena == ' ' or cadena == '.': # Fin de palabra
 
 		# Se asegura que sea una palabra valida.
-    if cont_letra > 1:  
+		if cont_letra > 1:  
 			cont_palabra +=1
-      # Si es una palabra valida y comienza con p. 
-      if flag_p : 
-        cont_palabras_p +=1
-  
+     
 		cont_letra = 0
+  # Si no es el fin de la palabra
+	else:  
+     #Primera letra que comienza con p. 
+		if cont_letra == 1 and cadena == 'p':
+			cont_palabras_p +=1
 
-print('Cantidad de palabras:', cont_palabra )		 
+print('Cantidad de palabras:', cont_palabra )		
+print('Cantidad de palabras que inician con p:', cont_palabras_p )	 
