87700b50420cd5a298ab482c2bb7ec46bff7e139
jjcenturion <jjcenturion00@gmail.com>
2020-01-17 15:47:20 +0000

    commit inicial

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 cont_letra = 0
 cont_palabra = 0
+cont_palabras_p = 0
 cadena = None
+flag_p = False
 
 while cadena !='.':
 	cadena = input('Ingresar letra, termina con"."')
 	cont_letra += 1
+   
+    #Primera letra que comienza con p. 
+  if cont_letra == 1 and cadena == 'p':
+    flag_p = True
 
-	if cadena == '' or cadena == '.':
-		if cont_letra > 1:
+	if cadena == '' or cadena == '.': # Fin de palabra
+
+		# Se asegura que sea una palabra valida.
+    if cont_letra > 1:  
 			cont_palabra +=1
+      # Si es una palabra valida y comienza con p. 
+      if flag_p : 
+        cont_palabras_p +=1
   
 		cont_letra = 0
 
-print('Cantidad de palabras:', cont_palabra )		
+print('Cantidad de palabras:', cont_palabra )		 
